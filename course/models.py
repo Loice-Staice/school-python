28ffd1869213dcc96cf00aa4652ca12616bebc6d
Loice-Staice <staiceloicea@gmail.com>
2024-07-25 07:27:04 +0300

    add migrations

--- course/models.py
+++ course/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
-    id = models.AutoField(primary_key=True)
+    # id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     # description = models.TextField()
     duration = models.IntegerField()
     # credits = models.IntegerField()
     
     def __str__(self):
-        return f"{self.name} {self.class_id}"
+        return f"{self.name}"
 
